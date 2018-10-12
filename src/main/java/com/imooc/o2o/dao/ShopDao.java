99f060f5cd61da334f577d3e67b94536fbb1be59
chudelong <1114553919@qq.com>
2018-10-12 09:11:59 +0800

    - 测试ShopDao类，ShopDao.xml

--- src/main/java/com/imooc/o2o/dao/ShopDao.java
+++ src/main/java/com/imooc/o2o/dao/ShopDao.java
@@ -1,9 +1,28 @@
 package com.imooc.o2o.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.imooc.o2o.entity.Shop;
 
 public interface ShopDao {
 
+	/**
+	 * 分页查询店铺，可输入的条件有：店铺名（模糊），店铺转态，店铺类别，区域Id,owner
+	 * @param shopCondition
+	 * @param rowIndex
+	 * @return
+	 */
+	List<Shop> queryShopList(@Param("shopCondition") Shop shopCondition, @Param("rowIndex") int rowIndex,
+			@Param("pageSize") int pageSize);
+	/**
+	 * 返回queryShopList总数
+	 * @param shopCondition
+	 * @return
+	 */
+	int queryShopCount(@Param("shopCondition")Shop shopCondition);
+	
 	/**
 	 * 通过shop id查询店铺
 	 * @param shopId
